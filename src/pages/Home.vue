029f80033cce246937638548ab40db8810a0e09d
Santiago Gallino <gallino.santi@gmail.com>
2024-11-29 12:32:04 -0300

    Cambia el nombre del sitio en Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,6 @@ import BaseHeading1 from '../components/BaseHeading1.vue';
 </script>
 
 <template>
-    <BaseHeading1>Página Principal</BaseHeading1>
+    <BaseHeading1>DV Social</BaseHeading1>
     <p>¡Les damos la bienvenida a la mejor red social de una institución educativa!</p>
 </template>
